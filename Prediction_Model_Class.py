5b47721cddb0ebe688321e90c4564f019602776f
Anderson <BMAnderson@mdanderson.org>
2020-04-20 08:44:10 -0500

    pull lung model

--- Prediction_Model_Class.py
+++ Prediction_Model_Class.py
@@ -158,7 +158,7 @@ def run_model(gpu=0):
         resize_class_256 = Resize_Images_Keras(num_channels=3)
         resize_class_512 = Resize_Images_Keras(num_channels=3, image_size=512)
         graph1 = Graph()
-        model_keys = ['liver_lobes','liver', 'lungs']
+        model_keys = ['liver_lobes','liver']
         with graph1.as_default():
             gpu_options = GPUOptions(allow_growth=True)
             for key in model_keys:
