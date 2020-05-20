fef0ef3d98f4951356680ed0676ad6b5a4523387
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-05-20 15:14:38 -0500

    only liver

--- Prediction_Model_Class.py
+++ Prediction_Model_Class.py
@@ -161,7 +161,7 @@ def run_model(gpu=0):
         resize_class_512 = Resize_Images_Keras(num_channels=3, image_size=512)
         graph1 = Graph()
         model_keys = ['liver_lobes','liver', 'lungs', 'liver_disease']
-        # model_keys = ['liver']
+        model_keys = ['liver']
         with graph1.as_default():
             gpu_options = GPUOptions(allow_growth=True)
             for key in model_keys:
