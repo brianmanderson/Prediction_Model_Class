aaddedebff6105834aea142dc9a57207825ef452
markba122@gmail.com <bmanderson@mdanderson.org>
2020-08-21 14:58:23 -0500

    pass along just mask

--- Image_Processing.py
+++ Image_Processing.py
@@ -495,7 +495,7 @@ class Mask_Prediction(Image_Processor):
             images[mask[..., 1] == 1] = inside
         sum_vals = np.zeros(mask.shape)
         sum_vals[..., 0] = 1 - mask[..., 0]
-        return [images, mask], annotations
+        return [images, annotations], annotations
 
     def post_process(self, images, pred, ground_truth=None):
         return images[0], pred, ground_truth
