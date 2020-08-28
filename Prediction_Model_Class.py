64a0635af03577bc6dc7d028bb75c7b2f244e25a
markba122@gmail.com <bmanderson@mdanderson.org>
2020-08-28 11:16:45 -0500

    turn off lobes, run locally

--- Prediction_Model_Class.py
+++ Prediction_Model_Class.py
@@ -219,7 +219,7 @@ def run_model():
         models_info['liver_disease'] = return_model_info(**model_info)
         all_sessions = {}
         graph = tf.compat.v1.Graph()
-        model_keys = ['liver_lobes', 'liver', 'lungs', 'parotid', 'liver_disease'] #liver_lobes
+        model_keys = ['liver', 'lungs', 'parotid', 'liver_disease'] #liver_lobes
         # model_keys = ['liver_lobes']
         with graph.as_default():
             gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
