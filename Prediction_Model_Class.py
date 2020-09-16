13789a795cd80f26dd2462858387f88cdeab9f8c
markba122@gmail.com <bmanderson@mdanderson.org>
2020-09-16 09:58:35 -0500

    all back on

--- Prediction_Model_Class.py
+++ Prediction_Model_Class.py
@@ -231,8 +231,8 @@ def run_model():
         models_info['liver_disease'] = return_model_info(**model_info)
         all_sessions = {}
         graph = tf.compat.v1.Graph()
-        model_keys = ['liver_lobes', 'liver', 'lungs', 'parotid']  # liver_lobes
-        # model_keys = ['liver_disease']
+        model_keys = ['liver_lobes', 'liver', 'lungs', 'parotid', 'liver_disease']  # liver_lobes
+        # model_keys = ['liver']
         with graph.as_default():
             gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
             for key in model_keys:
