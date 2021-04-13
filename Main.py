7bfdcecef6e8457a7cc2abe46405d66db00cd251
markba122@gmail.com <bmanderson@mdanderson.org>
2021-04-13 09:55:25 -0500

    attempt continual run

--- Main.py
+++ Main.py
@@ -10,4 +10,8 @@ print('\n\n\nRunning on {}\n\n\n'.format(gpu))
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 from Prediction_Model_Class import run_model
-run_model()
+while True:
+    try:
+        run_model()
+    except:
+        xxx = 1
