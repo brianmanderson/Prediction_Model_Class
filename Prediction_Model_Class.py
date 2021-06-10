b301dc7c266688eea1fac1542f15a0d8b6878d3d
Rigaud <BSRigaud@mdanderson.org>
2021-06-10 16:22:44 -0500

    add lacc in model keys

--- Prediction_Model_Class.py
+++ Prediction_Model_Class.py
@@ -79,8 +79,7 @@ def run_model():
             'lacc': return_lacc_model(),
         }
 
-        # model_keys = ['liver_lobes', 'liver', 'lungs', 'liver_disease']
-        model_keys = ['lacc']
+        model_keys = ['liver_lobes', 'liver', 'lungs', 'liver_disease','lacc']
 
         all_sessions = {}
         gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
