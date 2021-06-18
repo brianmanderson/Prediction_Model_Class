0378f2f1bc47dd1edd48eb78e8dcd15792f399de
Rigaud <BSRigaud@mdanderson.org>
2021-06-18 15:21:36 -0500

    use all GPU memory for all models and try/except

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ print('\n\n\nRunning on {}\n\n\n'.format(gpu))
 # GPU device
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
-os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = 'true'
+# os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = 'false'
 os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
 
 from Prediction_Model_Class import run_model
