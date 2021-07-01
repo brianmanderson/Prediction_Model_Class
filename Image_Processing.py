caf9a257057d393e9fcc04b462a4c379c0f06307
Rigaud <BSRigaud@mdanderson.org>
2021-07-01 14:46:24 -0500

    add Duodenum model and update CTVN model for 3 class and combined output

--- Image_Processing.py
+++ Image_Processing.py
@@ -433,10 +433,10 @@ def return_ctvn_model(add_version=True):
     ctvn_model = ModelBuilderFromTemplate(image_key='image',
                                           model_path=os.path.join(model_load_path,
                                                                   'CTVN',
-                                                                  'DLv3_model_CTVN_v0.hdf5'),
+                                                                  'DLv3_model_CTVN_v1.hdf5'),
                                           model_template=deeplabv3plus(input_shape=(512, 512, 3),
                                                                        backbone="xception",
-                                                                       classes=2, final_activation='softmax',
+                                                                       classes=3, final_activation='softmax',
                                                                        windowopt_flag=False,
                                                                        normalization='batch', activation='relu',
                                                                        weights=None).Deeplabv3())
@@ -459,19 +459,67 @@ def return_ctvn_model(add_version=True):
     ])
     ctvn_model.set_prediction_processors([
         ProcessPrediction(prediction_keys=('prediction',),
-                          threshold={"1": 0.35},
+                          threshold={"1": 0.5, "2":0.5},
+                          connectivity={"1": False, "2": False},
+                          extract_main_comp={"1": False, "2": False},
+                          thread_count=1, dist=50, max_comp=2),
+        CombinePredictions(prediction_keys=('prediction',), combine_ids=((1, 2),), closings=(True,)),
+    ])
+
+    if add_version:
+        roi_names = [roi + '_MorfeusLab_v0' for roi in ["CTVn", "CTV_PAN", "Nodal_CTV"]]
+    else:
+        roi_names = ["CTVn", "CTV_PAN", "Nodal_CTV"]
+
+    ctvn_model.set_dicom_reader(TemplateDicomReader(roi_names=roi_names))
+    return ctvn_model
+
+
+def return_duodenum_model(add_version=True):
+    morfeus_path, model_load_path, shared_drive_path, raystation_clinical_path, raystation_research_path = return_paths()
+    duodenum_model = ModelBuilderFromTemplate(image_key='image',
+                                              model_path=os.path.join(model_load_path,
+                                                                      'Duodenum',
+                                                                      'DLv3_model_Duodenum_v0.hdf5'),
+                                              model_template=deeplabv3plus(input_shape=(512, 512, 3),
+                                                                           backbone="xception",
+                                                                           classes=2, final_activation='softmax',
+                                                                           windowopt_flag=False,
+                                                                           normalization='batch', activation='relu',
+                                                                           weights=None).Deeplabv3())
+    paths = [
+        os.path.join(shared_drive_path, 'Duodenum_Auto_Contour', 'Input_3'),
+        os.path.join(morfeus_path, 'Auto_Contour_Sites', 'Duodenum_Auto_Contour', 'Input_3'),
+        os.path.join(raystation_clinical_path, 'Duodenum_Auto_Contour', 'Input_3'),
+        os.path.join(raystation_research_path, 'Duodenum_Auto_Contour', 'Input_3')
+    ]
+    duodenum_model.set_paths(paths)
+    duodenum_model.set_image_processors([
+        Normalize_Images(keys=('image',), mean_values=(25.0,), std_values=(129.0,)),
+        Threshold_Images(image_keys=('image',), lower_bounds=(-3.55,), upper_bounds=(3.55,), divides=(False,)),
+        AddByValues(image_keys=('image',), values=(3.55,)),
+        DivideByValues(image_keys=('image', 'image'), values=(7.10, 1 / 255)),
+        ExpandDimensions(axis=-1, image_keys=('image',)),
+        RepeatChannel(num_repeats=3, axis=-1, image_keys=('image',)),
+        Ensure_Image_Proportions(image_rows=512, image_cols=512, image_keys=('image',),
+                                 post_process_keys=('image', 'prediction')),
+    ])
+    duodenum_model.set_prediction_processors([
+        ProcessPrediction(prediction_keys=('prediction',),
+                          threshold={"1": 0.5},
                           connectivity={"1": False},
                           extract_main_comp={"1": False},
                           thread_count=1, dist=50, max_comp=2),
     ])
 
     if add_version:
-        roi_names = [roi + '_MorfeusLab_v0' for roi in ["CTVn"]]
+        roi_names = [roi + '_MorfeusLab_v0' for roi in ["Duodenum"]]
     else:
-        roi_names = ['CTVn']
+        roi_names = ['Duodenum']
 
-    ctvn_model.set_dicom_reader(TemplateDicomReader(roi_names=roi_names))
-    return ctvn_model
+    duodenum_model.set_dicom_reader(TemplateDicomReader(roi_names=roi_names))
+    return duodenum_model
+
 
 class BaseModelBuilder(object):
     def __init__(self, image_key='image', model_path=None, Bilinear_model=None, loss=None, loss_weights=None):
--- Prediction_Model_Class.py
+++ Prediction_Model_Class.py
@@ -5,7 +5,8 @@ from queue import *
 import time
 from Utils import cleanout_folder, down_folder
 from Image_Processing import return_liver_model, return_lung_model, return_liver_lobe_model, \
-    return_liver_disease_model, plot_scroll_Image, return_lacc_model, return_pancreas_model, return_ctvn_model
+    return_liver_disease_model, plot_scroll_Image, return_lacc_model, return_pancreas_model, return_ctvn_model, \
+    return_duodenum_model
 from Image_Processors_Module.src.Processors.MakeTFRecordProcessors import *
 import tensorflow as tf
 
@@ -75,9 +76,10 @@ def run_model():
             'lacc': return_lacc_model(),
             'pancreas': return_pancreas_model(),
             'ctvn': return_ctvn_model(),
+            'duodenum': return_duodenum_model(),
         }
 
-        model_keys = ['liver_lobes', 'liver', 'lungs', 'liver_disease', 'lacc', 'pancreas', 'ctvn']
+        model_keys = ['liver_lobes', 'liver', 'lungs', 'liver_disease', 'lacc', 'pancreas', 'ctvn', 'duodenum']
 
         for key in model_keys:
                 model_info = models_info[key]
@@ -205,9 +207,10 @@ def run_model_single(input_path, output_path, model_key):
             'lacc': return_lacc_model(),
             'pancreas': return_pancreas_model(),
             'ctvn': return_ctvn_model(),
+            'duodenum': return_duodenum_model(),
         }
 
-        model_list = ['liver', 'lungs', 'liver_lobes', 'liver_disease', 'lacc', 'pancreas', 'ctvn']
+        model_list = ['liver', 'lungs', 'liver_lobes', 'liver_disease', 'lacc', 'pancreas', 'ctvn', 'duodenum']
         if not model_key in model_list:
             raise ValueError('model_key should be one of {}'.format(model_list))
 
