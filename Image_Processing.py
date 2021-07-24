5d0cfcf3266674bbfad610c6c242ebae00a7963e
Rigaud <BSRigaud@mdanderson.org>
2021-07-24 15:33:26 -0500

    remove postprocess param

--- Image_Processing.py
+++ Image_Processing.py
@@ -513,7 +513,7 @@ def return_lacc_pb3D_model(add_version=True):
                                      "9": 0.5, "10": 0.5, "11": 0.5, "12": 0.5},
                           connectivity={"1": False, "2": True, "3": True, "4": False, "5": True, "6": False,
                                         "7": True, "8": True, "9": True, "10": True, "11": True, "12": True},
-                          extract_main_comp={"1": True, "2": False, "3": False, "4": True, "5": False, "6": True,
+                          extract_main_comp={"1": True, "2": False, "3": False, "4": True, "5": False, "6": False,
                                              "7": False, "8": False, "9": False, "10": False, "11": False, "12": False},
                           thread_count=12, dist=20, max_comp=2, min_vol=2000),
         CombinePredictions(prediction_keys=('prediction',), combine_ids=((7, 8),), closings=(False,)),
