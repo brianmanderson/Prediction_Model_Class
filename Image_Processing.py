d84ee7e8c2f96664918262f3207b64c4f7f1be2e
Rigaud <BSRigaud@mdanderson.org>
2021-08-23 14:57:30 -0500

    add 2D model template code and change trial id PSMA 3D

--- Image_Processing.py
+++ Image_Processing.py
@@ -732,14 +732,14 @@ def return_psma_pb3D_model(add_version=True):
     psma_model = PredictWindowSliding(image_key='image',
                                       model_path=os.path.join(model_load_path,
                                                               'PSMA_3D',
-                                                              'BasicUNet3D_Trial_0.hdf5'),
+                                                              'BasicUNet3D_Trial_1.hdf5'),
                                       model_template=BasicUnet3D(input_tensor=None, input_shape=required_size + (1,),
                                                                  classes=5, classifier_activation="softmax",
                                                                  activation="leakyrelu",
                                                                  normalization="group", nb_blocks=2,
                                                                  nb_layers=5, dropout='standard',
                                                                  filters=32, dropout_rate=0.1,
-                                                                 skip_type='att', bottleneck='standard',
+                                                                 skip_type='concat', bottleneck='standard',
                                                                  last_conv_name='Output_3D_Conv_custom').get_net(),
                                       nb_label=5, required_size=required_size
                                       )
@@ -779,7 +779,49 @@ def return_psma_pb3D_model(add_version=True):
                             mask_values=tuple([0 for i in range(1, 5)])),
         ProcessPrediction(prediction_keys=('prediction',),
                           threshold={"1": 0.5, "2": 0.5, "3": 0.5, "4": 0.5},
-                          connectivity={"1": True, "2": True, "3": False, "4": False},
+                          connectivity={"1": False, "2": False, "3": False, "4": False},
+                          extract_main_comp={"1": False, "2": False, "3": False, "4": False},
+                          dist={}, max_comp={}, min_vol={}, thread_count=4),
+    ])
+
+    if add_version:
+        roi_names = [roi + '_MorfeusLab_v4' for roi in
+                     ['Bladder', 'Rectum', 'Iliac Veins', 'Iliac Arteries']]
+    else:
+        roi_names = ['Bladder', 'Rectum', 'Iliac Veins', 'Iliac Arteries']
+
+    psma_model.set_dicom_reader(TemplateDicomReader(roi_names=roi_names))
+    return psma_model
+
+
+def return_psma_model(add_version=True):
+    morfeus_path, model_load_path, shared_drive_path, raystation_clinical_path, raystation_research_path = return_paths()
+    psma_model = ModelBuilderFromTemplate(image_key='image',
+                                          model_path=os.path.join(model_load_path,
+                                                                  'PSMA',
+                                                                  'DLv3_model_PSMA_Trial_0.hdf5'),
+                                          model_template=deeplabv3plus(input_shape=(512, 512, 1),
+                                                                       backbone="xception",
+                                                                       classes=5, final_activation='softmax',
+                                                                       windowopt_flag=True,
+                                                                       normalization='batch', activation='relu',
+                                                                       weights=None).Deeplabv3())
+    paths = [
+        os.path.join(shared_drive_path, 'PSMA_Auto_Contour', 'Input_3'),
+        os.path.join(morfeus_path, 'Auto_Contour_Sites', 'PSMA_Auto_Contour', 'Input_3'),
+        os.path.join(raystation_clinical_path, 'PSMA_Auto_Contour', 'Input_3'),
+        os.path.join(raystation_research_path, 'PSMA_Auto_Contour', 'Input_3')
+    ]
+    psma_model.set_paths(paths)
+    psma_model.set_image_processors([
+        ExpandDimensions(axis=-1, image_keys=('image',)),
+        Ensure_Image_Proportions(image_rows=512, image_cols=512, image_keys=('image',),
+                                 post_process_keys=('image', 'prediction')),
+    ])
+    psma_model.set_prediction_processors([
+        ProcessPrediction(prediction_keys=('prediction',),
+                          threshold={"1": 0.5, "2": 0.5, "3": 0.5, "4": 0.5},
+                          connectivity={"1": False, "2": False, "3": False, "4": False},
                           extract_main_comp={"1": False, "2": False, "3": False, "4": False},
                           dist={}, max_comp={}, min_vol={}, thread_count=4),
     ])
@@ -792,7 +834,6 @@ def return_psma_pb3D_model(add_version=True):
 
     psma_model.set_dicom_reader(TemplateDicomReader(roi_names=roi_names))
     return psma_model
-
 
 
 class BaseModelBuilder(object):
